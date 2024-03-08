a1af5be5ca6debc4db2ccea90e2e4ffee318b446
geisonbruno1 <geisonbruno0@gmail.com>
2024-03-07 21:01:38 -0300

    feat: Evento handleInputCepChange

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,70 +1,92 @@
 import Address from "../models/address.js";
-
+import * as addressService from "../services/address-service.js";
 
 function State() {
-    this.Address = new  Address();
-    
-    this.btnSave = null;
-    this.btnClear = null;
+  this.Address = new Address();
 
-    this.inputCep = null;
-    this.street = null;
-    this.inputNumber = null;
-    this.inputCity = null;
-    
-    this.errorCep = null;
-    this.errorNumber = null;
+  this.btnSave = null;
+  this.btnClear = null;
+
+  this.inputCep = null;
+  this.street = null;
+  this.inputNumber = null;
+  this.inputCity = null;
+
+  this.errorCep = null;
+  this.errorNumber = null;
 }
 
 const state = new State();
 
-
 export function init() {
+  state.inputCep = document.forms.newAddress.cep;
+  state.inputStreet = document.forms.newAddress.street;
+  state.inputNumber = document.forms.newAddress.number;
+  state.inputCity = document.forms.newAddress.city;
 
-    state.inputCep = document.forms.newAddress.cep;
-    state.inputStreet = document.forms.newAddress.street;
-    state.inputNumber = document.forms.newAddress.number;
-    state.inputCity = document.forms.newAddress.city;
+  state.btnSave = document.forms.newAddress.btnSave;
+  state.btnClear = document.forms.newAddress.btnClear;
 
-    state.btnSave = document.forms.newAddress.btnSave;  
-    state.btnClear = document.forms.newAddress.btnClear;  
+  state.errorCep = document.querySelector('[data-error="cep"]');
+  state.errorNumber = document.querySelector('[data-error="number"]');
 
-        state.errorCep = document.querySelector('[data-error="cep"]');
-        state.errorNumber = document.querySelector('[data-error="number"]');
+  state.inputNumber.addEventListener("change", handleInputNumberChange);
+  state.btnClear.addEventListener("click", handleBtnClearClick);
+  state.btnSave.addEventListener("click", handleBtnSaveClick);
+}
 
-        state.inputNumber.addEventListener('change', handleInputNumberChange);
-        state.btnClear.addEventListener('click', handleBtnClearClick);
+async function handleInputCepChange(event) {
+  const cep = event.target.value;
+  try {
+    const address = await addressService.findByCep(cep);
+
+    state.inputCity.value = address.city;
+    state.inputStreet.value = address.street;
+    state.address = address;
+
+    setFormError("cep", "");
+    state.inputNumber.focus();
+  } catch (e) {
+    state.inputStreet.value = "";
+    state.inputCity.value = "";
+    setFormError("cep", "Informe um CEP válido");
+  }
+}
+
+async function handleBtnSaveClick(event) {
+  event.preventDefault();
+  const result = await requestService.getJson(
+    "https://viacep.com.br/ws/01001000/json/"
+  );
+  console.log(result);
 }
 
 function handleInputNumberChange(event) {
-    if(event.target.value == "") {
-        setFormError("number", "Campo requirido");
-    }
-     else {
-        setFormError("number", "");
-     }
+  if (event.target.value == "") {
+    setFormError("number", "Campo requirido");
+  } else {
+    setFormError("number", "");
+  }
 }
 
 function handleBtnClearClick(event) {
-    event.preventDefault();
-    clearForm();
-
+  event.preventDefault();
+  clearForm();
 }
 
 function clearForm() {
-    state.inputCep.value = "";
-    state.inputCity.value = "";
-    state.inputNumber.value = "";
-    state.inputStreet.value = "";
-    
-    setFormError("cep", "");
-    setFormError("number", "");
-    
-    state.inputCep.focus();
+  state.inputCep.value = "";
+  state.inputCity.value = "";
+  state.inputNumber.value = "";
+  state.inputStreet.value = "";
+
+  setFormError("cep", "");
+  setFormError("number", "");
+
+  state.inputCep.focus();
 }
 
 function setFormError(key, value) {
-    const element = document.querySelector(`[data-error="${key}"]`)
-        element.innerHTML = value;
-
-    }
+  const element = document.querySelector(`[data-error="${key}"]`);
+  element.innerHTML = value;
+}
